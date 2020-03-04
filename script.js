989255e779ed45032f18938b9eeccf116c8497c8
Rjlintkh <samueltkho0331@gmail.com>
2020-03-04 11:14:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const url = "https://0bf1de9a.ngrok.io";
-location.assign(url + location.pathname);
+const domain_name = "https://0bf1de9a.ngrok.io";
+location.replace(domain_name + location.pathname);
